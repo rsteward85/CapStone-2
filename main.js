d244c6e5c55da5f5a690f4e5bc8b9c6768a64ed0
Ricardo Steward <rsteward93@gmail.com>
2023-05-24 09:07:40 -0400

    updated park data

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+/*/ This function is for loading the park data in the dropdown element on the search national parks page  */
 
+document.addEventListener("DOMContentLoaded", () => {
+  function populateParkDropdown(parkData) {
+    const dropPark = document.querySelector("#park");
+    for (const park of parkData) {
+      const option = document.createElement("option");
+      option.value = park;
+      option.innerText = park;
+      dropPark.appendChild(option);
+    }
+  }
+  populateParkDropdown(parkTypesArray);
+});
