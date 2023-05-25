e980f67b0e8795164fb71ad78f08d852f9ac089b
Ricardo Steward <rsteward93@gmail.com>
2023-05-24 22:39:37 -0400

    updates

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-/*/ This function is for loading the park data in the dropdown element on the search national parks page  */
+/*/ <!-- This function is for loading the park data in the dropdown element on the search national parks page 
+ - I had help with using the "this" value from https://github.com/LTillett
+        - --> */
 
 document.addEventListener("DOMContentLoaded", () => {
   function populateParkDropdown(parkData) {
@@ -11,4 +13,40 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
   populateParkDropdown(parkTypesArray);
+});
+
+function filterParkType(showMePark) {
+  const selectedPark = nationalParksArray.filter((objectOfPark) =>
+    objectOfPark.LocationName.includes(showMePark)
+  );
+  return selectedPark;
+}
+
+matchedState.forEach((element) => {
+  // Assigned variables from array
+  let localName = element.LocationName != null ? element.LocationName : "";
+  let localCity = element.City != null ? element.City : "";
+  let localState = element.State != null ? element.State : "";
+  let localAddress = element.Address != null ? element.Address : "";
+  let localVisit = element.Visit != null ? element.Visit : "";
+  let locationItem = document.createElement("p");
+
+  // Style <p> tags with CSS
+  locationItem.classList.add("parkBlock");
+
+  const args = [localName, localCity, localState, localAddress, localVisit];
+
+  function parkFormat() {
+    for (const arg of args) {
+      locationItem.appendChild(document.createTextNode(arg));
+      locationItem.appendChild(document.createElement("br"));
+    }
+  }
+  function parkTable(value) {
+    console.log("Selected park:", value);
+  }
+  const selectElement = document.querySelector("#park");
+  selectElement.addEventListener("change", function () {
+    parkTable(this.value);
+  });
 });
